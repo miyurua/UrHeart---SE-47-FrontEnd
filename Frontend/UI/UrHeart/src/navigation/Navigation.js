49a2889d66b7dd9683e4c3651c6abd51ab7be373
MohomedThariq <1234thariq@gmail.com>
2022-02-22 08:38:34 +0530

    React UI
    After installations of new dependencies and UI changes

---
+++ Frontend/UI/UrHeart/src/navigation/Navigation.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
+import DrawerNavigator from "./DrawerNavigator";
+
+import SignInScreen from "../screens/SignInScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import ConfirmEmailScreen from "../screens/ConfirmEmailScreen";
+import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
+import NewPasswordScreen from "../screens/NewPasswordScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+import Global from '../global';
+
+const Stack = createStackNavigator();
+
+const config = {
+  animation: "spring",
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
+const screenOptionStyle = {
+  headerShown: false,
+  gestureEnabled: true,
+  gestureDirection: "horizontal",
+  cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+  transitionSpec: { open: config, close: config },
+};
+
+const Navigation = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle} initialRouteName={Global.SignInState}>
+      <Stack.Screen name="SignIn" component={SignInScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+      <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+      <Stack.Screen name="Home" component={HomeScreen, DrawerNavigator} />
+    </Stack.Navigator>
+  );
+};
+
+export default Navigation;
