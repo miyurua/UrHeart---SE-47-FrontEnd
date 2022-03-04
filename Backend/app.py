99c67d286fdd3ac9e924a6af73ec4d49b02742a7
Abeysiriwardan <miuru.2019450@iit.ac.lk>
2022-03-04 18:20:53 +0530

    Created Basic Routes

--- Backend/app.py
+++ Backend/app.py
@@ -1,2 +1,32 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return {"Home Page": ["Profile", "Predict Heart Diseases", "Doctors List", "Upload ECG"]}, 200
+
+
+@app.route("/profile")
+def profile_page():
+    return {"Profile Page": ["Name", "Username", "Email", "Gender", "DOB"]}, 200
+
+
+@app.route("/predict-heart-disease")
+def medical_info_page():
+    return {"Predict Heart Disease Page": ["age", "gender", "chest pain type", "..."]},200
+
+
+@app.route("/doctors")
+def doctors_page():
+    return {"Doctors List": ["Doctor ID", "Doctor Name", "Hospital"]}
+
+
+@app.route("/ECG-Page")
+def ecg_page():
+    return {"ECG Upload": ["Browse"]}, 200
+
+
+if __name__ == '__main__':
+    app.run()
