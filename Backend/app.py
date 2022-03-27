5a3b79133aea1d32135e5e2d18f06249ac4a06d7
MohomedThariq <1234thariq@gmail.com>
2022-03-28 00:39:37 +0530

    response format

--- Backend/app.py
+++ Backend/app.py
@@ -27,14 +27,13 @@ def test():
 
 @app.route('/pred', methods = ['POST'])
 def pred():
-    body = request.json['body']
     test_data = request.json
     s1 = json.dumps(test_data)
     print_json = json.loads(s1)
     list = print_json['body']
     input_data = (float(list['age']), float(list['sex']), float(list['chestPainType']),	float(list['restingBP']), float(list['cholestrol']), float(list['fastingBloodSugar']), float(list['restingECG']), float(list['maxHeartRate']), float(list['exerciseAngina']),	float(list['oldpeak']), float(list['STslope']))
     print(input_data)
-    return jsonify(body)
+    return jsonify({'predResult':'1'})
 
 
 # if request.method == 'POST':
